2616e0e0a6b7a784a8a23ed1a2980c43a39d616e
David Bau <david.bau@gmail.com>
2022-03-23 21:00:17 -0400

    new widget and nethook functions.

--- baukit/__init__.py
+++ baukit/__init__.py
@@ -1,10 +1,12 @@
 from .labwidget import Model, Widget, Trigger, Property, Event
 from .labwidget import Button, Label, Textbox, Numberbox, Range, ColorPicker
 from .labwidget import Choice, Menu, Datalist, Div, ClickDiv, Image
+from .labwidget import Textarea
 from .paintwidget import PaintWidget
 from .plotwidget import PlotWidget
 from . import pbar
 from .nethook import Trace, TraceDict, set_requires_grad
+from .nethook import module_names, parameter_names
 from .nethook import subsequence, get_module, get_parameter, replace_module
 from .runningstats import Stat, Mean, Variance, Covariance, Bincount, CrossCovariance
 from .runningstats import IoU, CrossIoU, Quantile, TopK, History, CombinedStat
